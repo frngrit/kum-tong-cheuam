a5182bab4d0e069a295ee9efcc8bcfcea7d19c10
rerngrit <frank.12876@hotmail.com>
2024-09-27 22:05:01 +0700

    feat: add auto-correct

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ os.environ['PYTHAINLP_DATA_DIR'] = '/tmp'
 
 from flask import Flask, send_from_directory, request
 from pythainlp.tokenize import syllable_tokenize
+from pythainlp.spell import correct
 
 app = Flask(__name__)
 
@@ -15,7 +16,7 @@ def index():
 @app.route("/api/tokenize", methods=["POST"])
 def tokenize():
     data = request.json
-    word = data.get('word')
+    word = correct(data.get('word'))
     tokenized_syllables = syllable_tokenize(word, engine="dict")
     return {"syllables": [w for w in tokenized_syllables if w.strip() != ""]}
 

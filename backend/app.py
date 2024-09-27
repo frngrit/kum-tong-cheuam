81106cf67db94576342582dc15632583921448ff
rerngrit <frank.12876@hotmail.com>
2024-09-27 20:09:56 +0700

    feat: implement

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,20 @@
-from pythainlp.tokenize import syllable_tokenize,DEFAULT_SYLLABLE_TOKENIZE_ENGINE
+from flask import Flask, render_template, request
+from pythainlp.tokenize import syllable_tokenize
+from pythainlp.spell import correct
 
-EXIST_CODES = ["X", "x", "exit", "exit()"]
-word_dict = {}
+app = Flask(__name__)
 
-def add_new_word(raw_word):
-    tokenized_words = syllable_tokenize(raw_word, engine=DEFAULT_SYLLABLE_TOKENIZE_ENGINE)
-    for word in tokenized_words:
-        word_in_dict = word_dict.get(word)
-        if word_in_dict is not None:
-            return (False, word_in_dict, word)
-        
-        word_dict[word] = raw_word
-    return (True, "", "")
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
 
-while True:
-    raw_word = str(input("word: "))
-    
-    if raw_word in EXIST_CODES:
-        print("existing...")
-        break
-    
-    (is_valid, used_word, duplicate_on) = add_new_word(raw_word)
-    # print(word_dict)
-    if not is_valid:
-        print(f"used word detect: {used_word}, duplicated on: {duplicate_on}")
+# API endpoint to tokenize a word into syllables
+@app.route("/tokenize", methods=["POST"])
+def tokenize():
+    data = request.json
+    word = correct(data.get('word'))
+    tokenized_syllables = syllable_tokenize(word, engine="dict")
+    return {"syllables": tokenized_syllables}
+
+if __name__ == "__main__":
+    app.run(debug=True)
